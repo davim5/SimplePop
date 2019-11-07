b39422861fa7e3f8252fb239a2d8b1a200aaef31
Davi Lima <davim53@hotmail.com>
2019-11-07 08:06:09 -0300

    Loop for helicis and sound sensor check

--- pipocaAuto/pipocaAuto.ino
+++ pipocaAuto/pipocaAuto.ino
@@ -2,9 +2,10 @@
 #include <WiFi.h>
 #include <ESP32Servo.h>
 
+// ************************ WIFI SETUP ************************
 
-const char* ssid = "UNIFOR_WIFI";
-const char* password =  "";
+const char* ssid = "Aecio";
+const char* password =  "01030704";
 const char* mqttServer = "test.mosquitto.org";
 const int mqttPort = 1883;
 const char* mqttUser = "";
@@ -14,10 +15,21 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 Servo myservo;  // create servo object to control a servo
-//
+
+bool START = false;
+
+unsigned long TI;
+unsigned long TD;
+int contador;
+
+// ************************ DEFINIÇÃO DOS PINOS ************************
 
 int pos = 0;    // Posição do Servo
 int pinoSensor = 12;
+
+char str[10];
+
+  
 
 void setup() {
   myservo.attach(13);  // Servo ligado no pin 13
@@ -33,41 +45,96 @@ void setup() {
   }
   //  //
   Serial.println("Conectado na rede WiFi!");
+
 }
+
+//************************************ LOOP ************************************
 
 void loop() {
   reconectabroker();
 
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-//    Serial.println(pos);
-    myservo.write(pos);             // tell servo to go to position in variable 'pos'
-    delay(10);                       // waits 15ms for the servo to reach the position
-    if (digitalRead(pinoSensor) == HIGH) { //SE A LEITURA DO PINO FOR IGUAL A HIGH, FAZ
-      client.publish("/pipoca/mic", "1");
-      Serial.println("OUVIU");
-    }
-    else{
-      client.publish("/pipoca/mic", "0");
-      Serial.println("OUVIU NÃO");
-    }
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-//    Serial.println(pos);
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(10);                       // waits 15ms for the servo to reach the position
 
-    if (digitalRead(pinoSensor) == HIGH) { //SE A LEITURA DO PINO FOR IGUAL A HIGH, FAZ
-      client.publish("/pipoca/mic", "1");
-      Serial.println("Ouviu");
+  //GIRAR HELICES
+  //  if (START == true)
+  //  {
+  //    do {
+  //      Serial.println("Startou");
+  //
+  //      //  PEGAR TEMPO E ZERAR CONTADOR
+  //      //    OUVIR PIPOCADA
+
+  //      ACRESCENTAR CONTADOR
+
+  //      CHECAR TEMPO
+
+  //   CHECAR CONTADOR
+
+  //PARAR HELICES
+  //    } while (START == true);
+
+  contador = 0;
+  TI = millis();
+  //    OUVIR PIPOCADA
+  Serial.print("TI: ");
+  Serial.println(TI);
+
+
+  do {
+
+    sprintf(str, "%d", contador);
+    client.publish("/pipoca/mic", str);
+
+    for (pos = 0; pos <= 180; pos += 1) {
+      Serial.println(pos);
+      myservo.write(pos);
+      delay(20);
+      if (digitalRead(pinoSensor) == HIGH) {
+        contador++;
+        TD = millis();
+      }
+      else {
+        TD = millis();
+      }
     }
-    else{
-      client.publish("/pipoca/mic", "0");
-      Serial.println("OUVIU NÃO");
+    sprintf(str, "%d", contador);
+    client.publish("/pipoca/mic", str);
+    
+    for (pos = 180; pos >= 0; pos -= 1) {
+      Serial.println(pos);
+      myservo.write(pos);
+      delay(20);
+      if (digitalRead(pinoSensor) == HIGH) {
+        contador++;
+        TD = millis();
+      }
+      else {
+        TD = millis();
+      }
     }
+
+    sprintf(str, "%d", contador);
+    client.publish("/pipoca/mic", str);
+    
+    Serial.print("TD: ");
+    Serial.println(TD);
+
+    Serial.print("Contador chegou em: ");
+    Serial.println(contador);
+
+  } while ((TD - TI) < 10000);
+
+  if (contador > 10) {
+    Serial.println("Hora da pipoca! :)");
   }
+  else {
+    Serial.println("Pipoca ainda nao esta pronta! :(");
+  }
+
+
 }
 
+
+// ************************************************************  CONECXÃO COM O BROKER ************************************************************
 void reconectabroker() {
   //Conexao ao broker MQTT
   client.setServer(mqttServer, mqttPort);
